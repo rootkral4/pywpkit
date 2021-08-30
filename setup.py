f1040efda5bac650cd8d4ca6f795555e7f742db7
rootkral4 <vestel7676@hotmail.com>
2021-08-30 10:21:54 +0300

    minor regulations to setup.py, added url to setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup, find_packages
-
-VERSION = '0.0.1' 
-DESCRIPTION = 'Python package to interact with whatsapp'
-LONG_DESCRIPTION = 'Python package to interact with whatsapp using different methods'
+from pathlib import Path
 
 setup(
         name="wpkit", 
-        version=VERSION,
+        version='0.0.1',
         author="rootkral4",
         author_email="rootkral4@hotmail.com",
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
+        description='Python package to interact with whatsapp',
+        long_description=(Path(__file__).parent / "README.md").read_text(),
+        long_description_content_type='text/markdown',
         packages=find_packages(),
         install_requires=["keyboard"],
+        url='https://github.com/rootkral4/wpkit',
         keywords=['whatsapp', 'web whatsapp'],
 
 )
